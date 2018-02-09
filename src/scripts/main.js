c297aec0de88c52007bbb7ced3afbf5b8ea270d4
Romanf5 <berezhnoi.f5@gmail.com>
2018-02-09 18:58:19 +0200

    start tabs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
+  //UI Vars
+  var tabContent = $('.tab-content');
+  var tabControls = $('.tab-control');
+
+
+  initial();
+
+  //Initial function
+  function initial() {
+    tabControls.on('click', tabHandler);
+  }
+
+  //Tab logik
+  function tabHandler(event) {
+    event.preventDefault();
+    var targetItem = $(event.target);
+    var targetItemDataTab = $(event.target).attr('data-tab-control');
+    tabContent.removeClass('active-content');
+    targetItem.addClass('active-tab');
+    tabControls.not(this).removeClass('active-tab');
+
+    tabContent.each(function (index, element) {
+      var elementItem = $(element);
+      console.log(elementItem);
+      if(targetItemDataTab === elementItem.attr('data-tab-index')){
+        console.log(elementItem.attr('data-tab-index'));
+      }
+    })
+  }
 
 });
